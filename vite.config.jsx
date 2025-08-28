0052445fb7673b1f2b3b897b42675d63cfe5cb89
valoudefou <valerian.karsenty@gmail.com>
2025-08-28 11:08:31 +0300

    Update

--- vite.config.jsx
+++ vite.config.jsx
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import React, { useState, useEffect, useContext, createContext } from 'react';
-
 
 // https://vite.dev/config/
 export default defineConfig({
